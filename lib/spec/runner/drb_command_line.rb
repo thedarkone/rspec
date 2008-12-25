6e7612fcfcfae966800ea4493093204d843fcb3d
thedarkone <nobody>
2008-12-25 22:55:19 +0100

    Retry a couple of times before giving up. This is useful with automaticaly restarting spec_server (see my rspec-rails fork).

--- lib/spec/runner/drb_command_line.rb
+++ lib/spec/runner/drb_command_line.rb
@@ -7,11 +7,15 @@ module Spec
       # Runs specs on a DRB server. Note that this API is similar to that of
       # CommandLine - making it possible for clients to use both interchangeably.
       def self.run(options)
+        retry_count = 30
+        try_counter = 0
         begin
           DRb.start_service
           spec_server = DRbObject.new_with_uri("druby://127.0.0.1:8989")
           spec_server.run(options.argv, options.error_stream, options.output_stream)
         rescue DRb::DRbConnError => e
+          try_counter += 1
+          sleep(0.5) && retry if retry_count > try_counter
           options.error_stream.puts "No server is running"
         end
       end
